1a9643dbc1dbc925b8ca0ccedfde1c5a5d4a10e4
jack <18008258751@163.com>
2022-06-12 21:06:18 +0800

    feat: 追加标签导出功能

--- src/main/java/com/example/label/controller/LabelController.java
+++ src/main/java/com/example/label/controller/LabelController.java
@@ -1,19 +1,30 @@
 package com.example.label.controller;
 
+import com.alibaba.excel.EasyExcel;
+import com.alibaba.excel.ExcelWriter;
 import com.example.label.dto.LabelInput;
 import com.example.label.entity.Label;
 import com.example.label.repository.LabelRepository;
 import com.example.label.service.LabelService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.http.HttpHeaders;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.InputStream;
 
 @RestController
 @RequestMapping("/label")
 public class LabelController {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(LabelController.class);
 
     @Resource
     private LabelRepository labelRepository;
@@ -29,5 +40,22 @@ public class LabelController {
     @PostMapping
     public Long add(@Validated @RequestBody LabelInput labelInput) {
         return labelService.add(labelInput);
+    }
+
+    @GetMapping("/export")
+    public void export(HttpServletResponse response) {
+        response.setHeader(HttpHeaders.CONTENT_DISPOSITION, "attachment;filename=label.xlsx");
+        Page<Label> page = labelRepository.findAll(PageRequest.of(0, 10_000,
+                Sort.by(Sort.Direction.DESC, "createTime")));
+        try (InputStream template = getClass().getClassLoader().getResourceAsStream("template/label_export_template.xlsx")) {
+            ExcelWriter writer = EasyExcel.write(response.getOutputStream(), Label.class)
+                    .withTemplate(template)
+                    .needHead(false)
+                    .autoCloseStream(false).build();
+            writer.write(page.getContent(), EasyExcel.writerSheet().sheetNo(0).build());
+            writer.finish();
+        } catch (IOException e) {
+            LOGGER.error("Failed to export label.", e);
+        }
     }
 }
--- src/main/java/com/example/label/entity/Label.java
+++ src/main/java/com/example/label/entity/Label.java
@@ -1,7 +1,7 @@
 package com.example.label.entity;
 
-import com.example.label.enums.LabelAuth;
-import com.example.label.enums.LabelStatus;
+import com.alibaba.excel.annotation.ExcelIgnore;
+import com.alibaba.excel.annotation.ExcelProperty;
 import lombok.Data;
 import org.springframework.data.annotation.Id;
 
@@ -11,19 +11,27 @@ import java.util.Date;
 public class Label {
 
     @Id
+    @ExcelIgnore
     private Long id;
 
-    private String name;
-
+    @ExcelProperty(index = 0)
     private String code;
 
+    @ExcelProperty(index = 1)
+    private String name;
+
+    @ExcelProperty(index = 2)
     private String parentCode;
 
-    private Integer status;
-
+    @ExcelProperty(index = 3)
     private Integer auth;
 
+    @ExcelProperty(index = 4)
+    private Integer status;
+
+    @ExcelIgnore
     private Date createTime;
 
+    @ExcelIgnore
     private Date updateTime;
 }
